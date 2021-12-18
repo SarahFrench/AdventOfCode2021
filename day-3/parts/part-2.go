fcc835c43a1e09289366f95d78c665486acf58cb
Sarah French <sarah@sarahfrench.co.uk>
2021-12-18 19:25:38 +0000

    refactor day 3 part 2 solution

--- day-3/parts/part-2.go
+++ day-3/parts/part-2.go
@@ -9,13 +9,93 @@ import (
 )
 
 type Input struct {
-	eliminatedO2  bool
-	eliminatedCO2 bool
-	bits          []int
-	original      string
+	eliminated bool
+	bits       []int
+	original   string
 }
 
-// func processInputs(inputs map[int]Input)
+func getInputsCopy(originalMap, newMap map[int]Input) map[int]Input {
+	for k,v := range originalMap {
+		newMap[k] = v
+	  }
+	return newMap
+}
+
+func makeEliminatorFunction(eliminateOnMostCommon bool, tieDecider int) func(bool, int, int) bool {
+	return func(tie bool, mostCommonBit, value int) bool {
+		// Handle tie situations
+		if tie && value != tieDecider {
+			return true
+		}
+		// Handle decisions based on most common bit at position
+		if !tie && eliminateOnMostCommon && (value == mostCommonBit) {
+			return true
+		}
+		if !tie && !eliminateOnMostCommon && (value != mostCommonBit) {
+			return true
+		}
+		return false
+	}
+}
+
+func processInputs(inputs map[int]Input, shouldEliminate func(bool, int, int) bool) (rating int) {
+	for position := 0; position < len(inputs[0].bits); position++ {
+
+		// Create tally of bits, identify tie or winning bit
+		tally := map[int]int{
+			0: 0,
+			1: 0,
+		}
+
+		for k, v := range inputs {
+			if inputs[k].eliminated {
+				// Skip already-eliminated inputs
+				continue
+			}
+
+			tally[v.bits[position]] += 1
+		}
+
+		mostCommonBit := -1
+		tie := false
+		if tally[0] > tally[1] {
+			mostCommonBit = 0
+		}
+		if tally[0] < tally[1] {
+			mostCommonBit = 1
+		}
+		tie = tally[0] == tally[1]
+
+		if tally[0]+tally[1] == 1 {
+			// Only one considered input left; abort
+			break
+		}
+
+		// Eliminate inputs that fail conditions for this position
+		for k, v := range inputs {
+			if inputs[k].eliminated {
+				// Skip already-eliminated inputs
+				continue
+			}
+
+			eliminated := shouldEliminate(tie, mostCommonBit, v.bits[position])
+			v.eliminated = eliminated
+			inputs[k] = v
+		}
+	}
+
+	// Identify non-eliminated values
+	for _, v := range inputs {
+		if !v.eliminated {
+			decimal, err := strconv.ParseInt(v.original, 2, 64)
+			if err != nil {
+				log.Fatalf("cannot convert %s to an int", v.original)
+			}
+			rating = int(decimal)
+		}
+	}
+	return
+}
 
 func SolutionTwo(fileName string) (oxygenGeneratorRating int, co2ScrubberRating int) {
 	file, err := os.Open(fileName)
@@ -58,127 +138,21 @@ func SolutionTwo(fileName string) (oxygenGeneratorRating int, co2ScrubberRating 
 		log.Fatal(err)
 	}
 
-	// Find number with most popular bit at each position
-	for position := 0; position < len(inputs[0].bits); position++ {
+	// Get oxygen generator rating
+	// "...determine the most common value (0 or 1) in the current bit position, and keep only numbers with that bit in that position."
+	// "0 and 1 are equally common, keep values with a 1 in the position being considered"
+	oxygenEliminator := makeEliminatorFunction(false, 1)
+	tmp := map[int]Input{} // avoid mutating original `input` map
+	tmp = getInputsCopy(inputs, tmp)
+	oxygenGeneratorRating = processInputs(tmp, oxygenEliminator)
 
-		// Create tally of bits, identify tie or winning bit
-		tally := map[int]int{
-			0: 0,
-			1: 0,
-		}
+	// Get co2 scrubber rating
+	// "...determine the least common value (0 or 1) in the current bit position, and keep only numbers with that bit in that position."
+	// "If 0 and 1 are equally common, keep values with a 0 in the position being considered"	
+	co2Eliminator := makeEliminatorFunction(true, 0)
 
-		for k, v := range inputs {
-			if inputs[k].eliminatedO2 {
-				// Skip already-eliminated inputs
-				continue
-			}
-
-			tally[v.bits[position]] += 1
-		}
-
-		var winner int
-		tie := false
-		if tally[0] > tally[1] {
-			winner = 0
-		}
-		if tally[0] < tally[1] {
-			winner = 1
-		}
-		tie = tally[0] == tally[1]
-
-		if tally[0]+tally[1] == 1 {
-			// Only one contender left; abort
-			break
-		}
-
-		// Eliminate inputs that fail conditions for this position
-		for k, v := range inputs {
-			if inputs[k].eliminatedO2 {
-				// Skip already-eliminated inputs
-				continue
-			}
-
-			if tie && v.bits[position] == 0 {
-				v.eliminatedO2 = true
-			}
-
-			if !tie && v.bits[position] != winner {
-				v.eliminatedO2 = true
-			}
-			inputs[k] = v
-		}
-	}
-	for position := 0; position < len(inputs[0].bits); position++ {
-
-		// Create tally of bits, identify tie or winning bit
-		tally := map[int]int{
-			0: 0,
-			1: 0,
-		}
-
-		for k, v := range inputs {
-			if inputs[k].eliminatedCO2 {
-				// Skip already-eliminated inputs
-				continue
-			}
-
-			tally[v.bits[position]] += 1
-		}
-
-		var winner int
-		tie := false
-		if tally[0] > tally[1] {
-			winner = 0
-		}
-		if tally[0] < tally[1] {
-			winner = 1
-		}
-		tie = tally[0] == tally[1]
-
-		if tally[0]+tally[1] == 1 {
-			// Only one contender left; abort
-			break
-		}
-
-		// Eliminate inputs that fail conditions for this position
-		for k, v := range inputs {
-			if inputs[k].eliminatedCO2 {
-				// Skip already-eliminated inputs
-				continue
-			}
-
-			if tie && v.bits[position] == 1 {
-				v.eliminatedCO2 = true
-			}
-
-			if !tie && v.bits[position] == winner {
-				v.eliminatedCO2 = true
-			}
-			inputs[k] = v
-		}
-	}
-
-	// Identify non-eliminated values
-	for _, v := range inputs {
-		if !v.eliminatedO2 {
-			decimal, err := strconv.ParseInt(v.original, 2, 64)
-			if err != nil {
-				log.Fatalf("cannot convert %s to an int", v.original)
-			}
-			oxygenGeneratorRating = int(decimal)
-		}
-		if !v.eliminatedCO2 {
-			decimal, err := strconv.ParseInt(v.original, 2, 64)
-			if err != nil {
-				log.Fatalf("cannot convert %s to an int", v.original)
-			}
-			co2ScrubberRating = int(decimal)
-		}
-		if oxygenGeneratorRating > 0 && co2ScrubberRating > 0 {
-			// Both found
-			break
-		}
-	}
+	tmp = getInputsCopy(inputs, tmp)
+	co2ScrubberRating = processInputs(tmp, co2Eliminator)
 
 	return
 }
--- day-3/parts/part-2_test.go
+++ day-3/parts/part-2_test.go
@@ -14,19 +14,88 @@ func TestSolutionTwo(t *testing.T) {
 	oxygenGeneratorRating, co2ScrubberRating := SolutionTwo(fileName)
 
 	if oxygenGeneratorRating != expectedOxygen {
-		t.Errorf("Got: %d; wanted %d", oxygenGeneratorRating, expectedOxygen)
+		t.Errorf("Incorrect oxygen generator rating\nGot: %d; wanted %d", oxygenGeneratorRating, expectedOxygen)
 	} else {
 		t.Logf("Got the correct oxygen generator rating value!")
 	}
 	if co2ScrubberRating != expectedCO2 {
-		t.Errorf("Got: %d; wanted %d", co2ScrubberRating, expectedCO2)
+		t.Errorf("Incorrect CO2 scrubber rating\nGot: %d; wanted %d", co2ScrubberRating, expectedCO2)
 	} else {
 		t.Logf("Got the correct CO2 scrubber rating value!")
 	}
 	if oxygenGeneratorRating*co2ScrubberRating != expectedLifeSupport {
-		t.Errorf("Got: %d; wanted %d", oxygenGeneratorRating*co2ScrubberRating, expectedLifeSupport)
+		t.Errorf("Incorrect life support rating\nGot: %d; wanted %d", oxygenGeneratorRating*co2ScrubberRating, expectedLifeSupport)
 	} else {
 		t.Logf("Got the correct life support rating value!")
 	}
 
+}
+
+func TestMakeEliminatorFunction(t *testing.T) {
+
+	testCases := map[string]struct {
+		tie                   bool
+		tieDecider            int
+		eliminateOnMostCommon bool
+		mostCommonBit         int
+		value                 int
+		expectedFuncOutput    bool
+	}{
+		"In a tie, when a position's value matches the tie decider the input is kept": {
+			tie:                   true,
+			tieDecider:            1,
+			eliminateOnMostCommon: true,
+			mostCommonBit:         1,
+			value:                 1,
+			expectedFuncOutput:    false,
+		},
+		"In a tie, when a position's value DOESN'T match the tie decider the input is eliminated": {
+			tie:                   true,
+			tieDecider:            1,
+			eliminateOnMostCommon: true,
+			mostCommonBit:         1,
+			value:                 0,
+			expectedFuncOutput:    true,
+		},
+		"When the value differs from the mostCommonBit and eliminateOnMostCommon is TRUE, the input is kept": {
+			tie:                   false,
+			tieDecider:            1,
+			eliminateOnMostCommon: true,
+			mostCommonBit:         1,
+			value:                 0,
+			expectedFuncOutput:    false,
+		},
+		"When the value matches the mostCommonBit and eliminateOnMostCommon is TRUE, the input is eliminated ": {
+			tie:                   false,
+			tieDecider:            1,
+			eliminateOnMostCommon: true,
+			mostCommonBit:         1,
+			value:                 1,
+			expectedFuncOutput:    true,
+		},
+		"When the value matches the mostCommonBit and eliminateOnMostCommon is FALSE, the input is kept": {
+			tie:                   false,
+			tieDecider:            1,
+			eliminateOnMostCommon: false,
+			mostCommonBit:         1,
+			value:                 1,
+			expectedFuncOutput:    false,
+		},
+		"When the value differs from the mostCommonBit and eliminateOnMostCommon is FALSE, the input is eliminated": {
+			tie:                   false,
+			tieDecider:            1,
+			eliminateOnMostCommon: false,
+			mostCommonBit:         1,
+			value:                 0,
+			expectedFuncOutput:    true,
+		},
+	}
+	for testName, tc := range testCases {
+		outputFunc := makeEliminatorFunction(tc.eliminateOnMostCommon, tc.tieDecider)
+		output := outputFunc(tc.tie, tc.mostCommonBit, tc.value)
+
+		if output != tc.expectedFuncOutput {
+			t.Errorf("FAIL: %s\nGot %t, expected %t", testName, output, tc.expectedFuncOutput)
+		}
+	}
 }
